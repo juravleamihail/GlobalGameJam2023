4497158a8a1c453782d350706a8b625af9260cb8
mihai2501 <mihai2501@gmail.com>
2023-02-04 21:10:23 +0200

    small animation fade try hard on plus atack

--- Assets/_Scripts/Weapons/DestroyWeapon.cs
+++ Assets/_Scripts/Weapons/DestroyWeapon.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DestroyWeapon : MonoBehaviour
 {
+	public Animation anim;
+	
 	private void OnEnable()
 	{
+		if (anim!=null)
+		{
+			anim.Play();
+		}
+		
 		StartCoroutine(SetObjectInactive(0.2f));
 	}
 
